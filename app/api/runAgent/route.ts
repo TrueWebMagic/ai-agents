9e6507963c56b3583a81cc074bea012654e4e3fd
TrueWebMagic <nicholaspiech@gmail.com>
2024-11-29 22:32:17 +0100

    tttttt

--- app/api/runAgent/route.ts
+++ app/api/runAgent/route.ts
@@ -1,11 +1,14 @@
+import { runAgent } from '@/app/utils/runAgent';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
     console.log(request);
+    runAgent("59")
     return NextResponse.json({ message: 'hi' });
 }
 
 export async function POST(request: Request) {
     console.log(request);
+    runAgent("59")
     return NextResponse.json({ message: 'hi' });
 }
