0082cf4f3fde9a1013b57b7a52a3f50fb2accf67
TrueWebMagic <nicholaspiech@gmail.com>
2024-11-29 21:50:43 +0100

    test4

--- app/api/runAgent/route.ts
+++ app/api/runAgent/route.ts
@@ -2,3 +2,8 @@ export async function GET(request: Request) {
     console.log(request)
     return new Response('hi')
 }
+
+export async function POST(request: Request) {
+    console.log(request)
+    return new Response('hi')
+}
