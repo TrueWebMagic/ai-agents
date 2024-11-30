56df9391d3b13d20b39f818499264727aa6e9199
TrueWebMagic <nicholaspiech@gmail.com>
2024-11-30 17:06:34 +0100

    scheduler t10

--- app/api/create-scheduler/route.ts
+++ app/api/create-scheduler/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
         }
 
         const client = new CloudSchedulerClient({
-            keyFilename: '../../../../secret.json',
+            // keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
         });
 
         const projectId = process.env.GOOGLE_PROJECT_ID!;
